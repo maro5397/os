f4ed89fb941ad8b7093f9f4a374a94a59b86f0cc
reindeer002 <lsj000424@gmail.com>
2026-01-03 23:16:36 +0900

    feat: print string when boot loader is uploaded

--- boot-loader/boot-loader.asm
+++ boot-loader/boot-loader.asm
@@ -3,13 +3,40 @@
 
 SECTION .text
 
-mov ax, 0xB800
-mov ds, ax
+; Far jump to force CS:IP to a known state.
+; This handles BIOS variants that load to 0000:7C00 instead of 07C0:0000.
+jmp 0x07c0:START
 
-mov byte [ 0x00 ], 'M'
-mov byte [ 0x01 ], 0x4A
+START:
+    mov ax, 0x07c0
+    mov ds, ax
+    mov ax, 0xb800
+    mov es, ax
+    mov si, 0
 
-jmp $
+; Local label to prevent naming collisions
+.SCREENCLEARLOOP:
+    mov byte [ es: si ], 0
+    mov byte [ es: si + 1 ], 0x0a
+    add si, 2
+    cmp si, 80 * 25 * 2 ; size of screen
+    jl .SCREENCLEARLOOP
+    mov si, 0
+    mov di, 0
+
+.MESSAGELOOP:
+    mov cl, byte [ si + MESSAGE1 ]
+    cmp cl, 0
+    je .MESSAGEEND
+    mov byte [ es: di ], cl
+    add si, 1
+    add di, 2
+    jmp .MESSAGELOOP
+
+.MESSAGEEND:
+    jmp $
+
+MESSAGE1: db 'REIN OS Boot Loader Start~!!', 0
 
 times 510 - ( $ - $$ ) db 0x00
 db 0x55
